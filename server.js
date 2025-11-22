fe78e807c1f706752f66d28eba42bd40cc233081
Aditya Raorane <145953001+AdityaRaorane22@users.noreply.github.com>
2025-11-22 17:47:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(cors({
-  origin: "https://stockmaster-backend-1oxp.onrender.com/",
+  origin: "https://stockmaster-frontend.vercel.app/",
   credentials: true
 }));
 app.use(express.json());
